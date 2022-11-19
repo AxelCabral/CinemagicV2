40d90b63e3bf89133c3869ea92dec0760cb3d6f0
Matheus Fialho <matheuslf25@gmail.com>
2022-11-19 20:45:22 -0300

    testeFrontBack

--- front-end/cinemagic/src/pages/Home/Home.tsx
+++ front-end/cinemagic/src/pages/Home/Home.tsx
@@ -1,12 +1,31 @@
 import '../../index.css';
 import Cartaz from './cartaz/Cartaz';
+import { movieApi } from '../../lib/axios';
 
-export default function Home() {
-  
+interface HomeProps {
+    movieId?: string,
+    movieTitle?: string,
+    movieRD?: Date,
+    movieLIM?: number,
+    movieURL?: string
+}
+
+export const getServerSideProps = async () => {
+	try {
+		const response = await movieApi.get("/movies");
+		
+	}
+	catch (error) {
+		console.log(error);
+	}
+}
+
+export default function Home(props: HomeProps) {
+    getServerSideProps()
     return (
             <div className="corpo-home">
                 <div className="card bg-dark">
-                    <Cartaz text = "https://br.web.img3.acsta.net/pictures/22/09/08/09/16/4886164.jpg" />
+                    <h1></h1><Cartaz text = {""}/>
                     <Cartaz text = "https://portalpopline.com.br/wp-content/uploads/2022/10/poster-pantera-negra-1.jpg" />
                     <Cartaz text = "https://pipocanamadrugada.com.br/site/wp-content/uploads/2022/05/Avatar-O-Caminho-da-Agua.jpg" />
                 </div>
